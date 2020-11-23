536cde06b55a6d0bc84e4a572ff62582c2af254b
Gelios <Geliosal39hon@gmail.com>
2020-11-23 10:15:51 +0300

    add siblings, closest

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,8 @@ $('div').click(function() {
     console.log($(this).index());
 });
 
-console.log($('div').eq(2).find('.more'));
+//console.log($('div').eq(2).find('.more'));
+//console.log($('.some').closest('.findme').addClass('123'));
+console.log($('.more').eq(0).siblings());
 
 //console.log($('button').html('hello'));
