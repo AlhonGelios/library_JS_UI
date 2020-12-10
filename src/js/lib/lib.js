109ec54e519adf4ed72e6538c4f64f759bed35ba
Gelios <Geliosal39hon@gmail.com>
2020-12-10 18:21:08 +0300

    add card, moda, button

--- src/js/lib/lib.js
+++ src/js/lib/lib.js
@@ -4,5 +4,7 @@ import './modules/classes';
 import './modules/action';
 import './modules/handlers';
 import './modules/effects';
+import './components/dropdown';
+import './components/modal';
 
 export default $;
